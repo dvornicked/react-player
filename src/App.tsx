a0937739fce8341f9018a5afb730757e2c1def90
Dvornick <mail@dvornick.ru>
2022-07-02 18:31:57 +0300

    Add React-Player

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from 'react'
+import ReactPlayer from 'react-player/lazy'
 
-function App() {
+export const App = () => {
+  const playerRef = useRef<ReactPlayer>(null)
+  const [isReady, setReady] = useState(false)
+
+  const changeTime = (seconds: number) => {
+    if (!playerRef.current) return
+    const currentTime = playerRef.current.getCurrentTime()
+    playerRef.current.seekTo(currentTime + seconds)
+  }
+
+  const onChangeBitrate = (event: any) => {
+    const internalPlayer = playerRef.current?.getInternalPlayer('hls')
+    if (internalPlayer) {
+      internalPlayer.currentLevel = event.target.value
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <ReactPlayer
+        ref={playerRef}
+        controls
+        onReady={() => setReady(true)}
+        onProgress={progress =>
+          console.log(playerRef.current?.getInternalPlayer('hls'))
+        }
+        config={{
+          file: {
+            forceHLS: true,
+          },
+        }}
+        url='http://sample.vodobox.net/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8'
+      />
+      <div>
+        <button onClick={() => changeTime(10)}>+10s</button>
+        Quality:
+        {isReady && (
+          <select onChange={onChangeBitrate}>
+            {playerRef.current
+              ?.getInternalPlayer('hls')
+              ?.levels.map((level: any, id: any) => (
+                <option key={id} value={id}>
+                  {level.bitrate}
+                </option>
+              ))}
+          </select>
+        )}
+      </div>
+    </>
+  )
 }
-
-export default App;
